249630e16c2f1937d55e85713a76b2188886a86e
Chassidy90 <Chassidykay90@gmail.com>
2023-09-28 20:26:34 -0500

    class 11

---
+++ Class 11/my-first-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'Chassidy';
+  quote ='';
+  boxes = [0, 0, 0];
+}
