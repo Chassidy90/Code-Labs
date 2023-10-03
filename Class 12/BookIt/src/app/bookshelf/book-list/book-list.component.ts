a7607717146b787380a545b09cf8aa0c927a5e73
Chassidy90 <Chassidykay90@gmail.com>
2023-10-02 20:18:16 -0500

    class 12

---
+++ Class 12/BookIt/src/app/bookshelf/book-list/book-list.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Book } from 'src/app/shared/book/book.model';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent {
+myBooks: Book [] = [
+  new Book ('Harry Potter',
+  'JKR',
+  'Fantasy',
+  'https://cdn.vox-cdn.com/thumbor/VnKewmsq_3QjD6XxQ7M6lZkk1wc=/0x0:1920x1080/1820x1213/filters:focal(755x89:1061x395):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/72144783/harrypotter.0.jpg'),
+  new Book ('The Cat In The Hat',
+  'Dr, Seuss',
+  'Childrens',
+  'https://th.bing.com/th/id/R.02d41d23646b95dd989e3c042dc57194?rik=asQkvo%2frq5H9bQ&riu=http%3a%2f%2fwww.weekendnotes.com%2fim%2f007%2f09%2fthe-cat-in-the-hat11.jpg&ehk=bc47ghfbJctccShe%2f5OMNBbQc2zOT7B4vdhfzEC4qjY%3d&risl=&pid=ImgRaw&r=0'),
+  new Book ('Hatchet',
+  'Gary Paulsen',
+  'Young Adult',
+  'https://cdn.vox-cdn.com/thumbor/VnKewmsq_3QjD6XxQ7M6lZkk1wc=/0x0:1920x1080/1820x1213[…]cdn.com/uploads/chorus_image/image/72144783/harrypotter.0.jpg')
+]
+}
--- Follow-Along App/Follow-Along-App/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Follow-Along App/Follow-Along-App/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+recipes:recipe[] = [
+  new recipe('A Test Recipe','This is simply a test', 'https://i0.wp.com/www.livewellbakeoften.com/wp-content/uploads/2018/01/Banana-Nut-Bread.jpg?resize=1360,2040')
+];
+constructor() {}
+ngOnInit(): void {
 
+}
 }
